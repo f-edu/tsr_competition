d8d853bfb6bc21bd06b5ef0d4dc3924f54128600
kuskovstanislav <kuskov.stanislav@gmail.com>
2018-08-09 09:55:57 +0300

    Write wideo from camera and save video as frames

---
+++ tools/video_to_frames.py
@@ -0,0 +1,24 @@
+import cv2
+
+video_name = 'test2.mp4'
+cap = cv2.VideoCapture('video/' + video_name)
+
+
+frame_id = 0
+
+while cap.isOpened():
+
+    ret, frame = cap.read()
+
+    if frame_id > 200:
+        cv2.imwrite('images/dataset/'+ str(frame_id) + ".jpg", frame)
+        # cv2.imshow("frame", frame)
+    if frame_id > 1000:
+        break
+    frame_id += 1
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
